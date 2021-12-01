1cba61aea1963aef705d63273215f6492b369662
Rustam Rustamov <rustam.r.r.r.r@gmail.com>
2021-12-01 15:35:28 +0200

    first

---
+++ HW10/main.js
@@ -0,0 +1,52 @@
+let pas1 = document.getElementById('pas1')
+let pas = document.getElementById('pas')
+let i = document.getElementById('i')
+let i1 = document.getElementById('i1')
+let btn = document.querySelector('.btn')
+
+
+function slash(event) {
+
+    if (i.classList.contains('fa-eye')) {
+        i.classList.remove('fa-eye')
+        i.classList.add('fa-eye-slash')
+        pas.setAttribute('type', 'text')
+    } else {
+        i.classList.remove('fa-eye-slash')
+        i.classList.add('fa-eye')
+        pas.setAttribute('type', 'password')
+    }
+
+
+}
+
+function slash1(event) {
+
+    if (i1.classList.contains('fa-eye')) {
+        i1.classList.remove('fa-eye')
+        i1.classList.add('fa-eye-slash')
+        pas1.setAttribute('type', 'text')
+    } else {
+        i1.classList.remove('fa-eye-slash')
+        i1.classList.add('fa-eye')
+        pas1.setAttribute('type', 'password')
+    }
+
+}
+function press (event) {
+    if (((pas.value !== '') && (pas1.value !== '')) && (pas.value === pas1.value)) {
+        alert('You are welcome')
+    }else {
+        let warning = document.createElement('p')
+        warning.innerHTML = 'Нужно ввести одинаковые значения'
+        warning.style.color = 'red'
+        pas1.after(warning)
+    }
+}
+
+
+
+i.addEventListener('click', slash)
+i1.addEventListener('click', slash1)
+btn.addEventListener('click', press)
+
--- HW9/main.js
+++ HW9/main.js
@@ -1,25 +1,11 @@
-// const tabs = document.querySelector('.tabs');
-// const cont = document.querySelector('.cont')
-// const button = document.querySelectorAll('.tabs-title')
-// const tabContent = document.querySelector('.tabs-content')
+
 const tabs = document.getElementsByClassName('tabs');
-// const cont = document.getElementsByClassName('cont')
+const cont = document.getElementsByClassName('cont')
 const button = document.getElementsByClassName('tabs-title')
 const tabContent = document.getElementsByClassName('cont')
 button[0].classList.add('active');
 tabContent[0].style.display = 'block';
-// button.forEach((element) => {
-//     element.addEventListener('click', function (event) {
-//         element.classList.add('active')
-//         console.log(event.target)
-//     })
-// })
-// button.forEach((element) => {
-//     element.addEventListener('mouseout', function () {
-//         element.classList.remove('active')
-//
-//     })
-// })
+
 function tab(event,name) {
         for (let i = 0; i < button.length; i++) {
             tabContent[i].style.display = 'none';
@@ -28,11 +14,3 @@ function tab(event,name) {
         document.getElementById(name).style.display = 'block';
         event.currentTarget.classList.add('active');
 }
-
-// tit.forEach((element) => {
-//     element.addEventListener('click', function (event) {
-//         function show(event) {
-//             event.target.classList.add('on')
-//         }
-//     })
-// button
