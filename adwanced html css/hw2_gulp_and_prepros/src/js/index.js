b4b0e3722af7720514ca081119985aa6fb90fe26
Rustam Rustamov <rustam.r.r.r.r@gmail.com>
2022-01-31 21:22:31 +0200

    the debug

--- adwanced html css/hw2_gulp_and_prepros/src/js/index.js
+++ adwanced html css/hw2_gulp_and_prepros/src/js/index.js
@@ -2,9 +2,21 @@
 const btnMenu = document.querySelector(".navbar__burger");
 const menu = document.querySelector(".navbar__list");
 const head = document.querySelector('.header:before')
+const bod = document.querySelector('html');
+
+
+
 
 btnMenu.addEventListener("click", () => {
     btnMenu.classList.toggle("active");
     menu.classList.toggle("active");
-    head.style.top = '-100%';
+
+
+});
+
+bod.addEventListener("click", () => {
+    const actives = document.getElementsByClassName('active');
+    const currentActive = actives[0];
+    if (currentActive)
+        currentActive.classList.remove("active");
 });
